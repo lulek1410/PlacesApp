ab95c58eb248bf23e08706ed402038951092e284
lulek1410 <jan.szewczynski@gmail.com>
2024-03-21 22:13:54 +0100

    (feat): Add auto authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   Outlet,
   RouterProvider,
@@ -77,19 +77,56 @@ const loggedInRouter = createBrowserRouter([
   },
 ]);
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState(null);
   const [userId, setUserId] = useState(null);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, exporationDate) => {
     setToken(token);
     setUserId(uid);
+    const expiration =
+      exporationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(expiration);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token,
+        expiration: expiration.toISOString(),
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      userData &&
+      userData.token &&
+      new Date(userData.expiration) > new Date()
+    ) {
+      login(userData.userId, userData.token, new Date(userData.expiration));
+    }
+  }, [login]);
 
   return (
     <AuthContext.Provider
