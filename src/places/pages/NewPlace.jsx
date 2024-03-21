8e76ab2408c63ad7f034cf83994c51a06bbc139c
lulek1410 <jan.szewczynski@gmail.com>
2024-03-21 23:10:37 +0100

    (feat): Remove passing of redundant property

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -39,7 +39,6 @@ export const NewPlace = () => {
       formData.append("title", formState.inputs.title.value);
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
-      formData.append("creator", userId);
       formData.append("image", formState.inputs.image.value);
       await sendRequest("http://localhost:5000/api/places", "POST", formData, {
         Authorization: "Bearer " + token,
