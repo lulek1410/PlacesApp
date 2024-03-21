6837d0e385d32f7e1174642ccb0a73947acb3cb4
lulek1410 <jan.szewczynski@gmail.com>
2024-03-21 22:23:23 +0100

    (fix): Fix typo

--- src/places/components/PlaceList.jsx
+++ src/places/components/PlaceList.jsx
@@ -7,28 +7,28 @@ import "./PlaceList.css";
 import Button from "../../shared/FormElements/Button";
 
 export const PlaceList = (props) => {
-	return props.items.length === 0 ? (
-		<div className="place-list center">
-			<Card>
-				<h2>No places found. Maybe create one?</h2>
-				<Button to="/places/new">Share Place</Button>
-			</Card>
-		</div>
-	) : (
-		<ul className="place-list">
-			{props.items.map((place) => (
-				<PlaceItem
-					key={place.id}
-					id={place.id}
-					image={place.image}
-					title={place.title}
-					description={place.desctiption}
-					address={place.address}
-					creatorId={place.creator}
-					coordinates={place.location}
-					onDelete={props.onDeletePlace}
-				/>
-			))}{" "}
-		</ul>
-	);
+  return props.items.length === 0 ? (
+    <div className="place-list center">
+      <Card>
+        <h2>No places found. Maybe create one?</h2>
+        <Button to="/places/new">Share Place</Button>
+      </Card>
+    </div>
+  ) : (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.image}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+          onDelete={props.onDeletePlace}
+        />
+      ))}{" "}
+    </ul>
+  );
 };
