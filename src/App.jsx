9c4a75f1b2ae05316c9747c6c0e9933f97b16dff
lulek1410 <jan.szewczynski@gmail.com>
2024-03-21 22:23:43 +0100

    (feat): Extract auto auth logic to custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import {
+  Navigate,
   Outlet,
   RouterProvider,
   createBrowserRouter,
-  Navigate,
 } from "react-router-dom";
+
 import { NewPlace } from "./places/pages/NewPlace";
 import { UpdatePlace } from "./places/pages/UpdatePlace";
 import { UserPlaces } from "./places/pages/UserPlaces";
-import { MainNavigation } from "./shared/components/Navigation/MainNavigation";
 import { Auth } from "./user/pages/Auth";
 import { Users } from "./user/pages/Users";
+
+import { MainNavigation } from "./shared/components/Navigation/MainNavigation";
+
 import { AuthContext } from "./shared/context/auth-context";
+
+import { useAuth } from "./shared/hooks/auth-hook";
 
 const Layout = () => {
   return (
@@ -77,57 +82,9 @@ const loggedInRouter = createBrowserRouter([
   },
 ]);
 
-let logoutTimer;
-
 const App = () => {
-  const [token, setToken] = useState(null);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState(null);
-  const [userId, setUserId] = useState(null);
-
-  const login = useCallback((uid, token, exporationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const expiration =
-      exporationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(expiration);
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uid,
-        token,
-        expiration: expiration.toISOString(),
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-    setTokenExpirationDate(null);
-    localStorage.removeItem("userData");
-  }, []);
-
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      const remainingTime =
-        tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpirationDate]);
-
-  useEffect(() => {
-    const userData = JSON.parse(localStorage.getItem("userData"));
-    if (
-      userData &&
-      userData.token &&
-      new Date(userData.expiration) > new Date()
-    ) {
-      login(userData.userId, userData.token, new Date(userData.expiration));
-    }
-  }, [login]);
-
+  const { login, logout, token, userId } = useAuth();
+  
   return (
     <AuthContext.Provider
       value={{ isLoggedIn: !!token, token, userId, login, logout }}
---
+++ src/shared/hooks/auth-hook.js
@@ -0,0 +1,55 @@
+import { useCallback, useEffect, useState } from "react";
+
+let logoutTimer;
+
+export const useAuth = () => {
+  const [token, setToken] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState(null);
+  const [userId, setUserId] = useState(null);
+
+  const login = useCallback((uid, token, exporationDate) => {
+    setToken(token);
+    setUserId(uid);
+    const expiration =
+      exporationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(expiration);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token,
+        expiration: expiration.toISOString(),
+      })
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem("userData");
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      userData &&
+      userData.token &&
+      new Date(userData.expiration) > new Date()
+    ) {
+      login(userData.userId, userData.token, new Date(userData.expiration));
+    }
+  }, [login]);
+
+  return { login, logout, token, userId };
+};
